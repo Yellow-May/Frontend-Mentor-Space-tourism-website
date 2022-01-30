5f01e435a38cb945c65fd09a4d3ad661422a5c0e
Yellow May <kwerep09@yahoo.com>
2022-01-30 21:23:20 +0100

    📦 NEW: technology page

--- pages/technology.jsx
+++ pages/technology.jsx
@@ -1,3 +1,62 @@
+import * as React from 'react';
+import Image from 'next/image';
+import { useRouter } from 'next/router';
+
+import allData from '../data/data.json';
+import useWindowDimensions from '../hooks/use-window-dimensions';
+
 export default function TechnologyPage() {
-	return <main></main>;
+	const router = useRouter();
+	const currentUrl = router.pathname.substring(1);
+
+	// nav state
+	const [curr, setCurr] = React.useState(0);
+
+	// data state
+	const data = React.useMemo(() => {
+		return allData[currentUrl][curr];
+	}, [curr, currentUrl]);
+
+	// get current screen size
+	const { width } = useWindowDimensions();
+
+	return (
+		<main className='technology-page'>
+			<h1>
+				<span>03</span> Space launch 101
+			</h1>
+
+			<div>
+				<div>
+					<Image
+						src={width >= 1024 ? data?.images.portrait : data?.images.landscape}
+						alt={data?.name}
+						layout='fill'
+						priority
+					/>
+				</div>
+
+				<div>
+					<nav>
+						<ul>
+							{[...Array(allData[currentUrl]?.length).keys()].map(i => (
+								<li key={i} className={curr === i ? 'current' : ''}>
+									<button onClick={() => setCurr(i)}>{i + 1}</button>
+								</li>
+							))}
+						</ul>
+					</nav>
+
+					<div>
+						<div>
+							<span>The terminology...</span>
+							<h2>{data?.name}</h2>
+						</div>
+
+						<p>{data?.description}</p>
+					</div>
+				</div>
+			</div>
+		</main>
+	);
 }
